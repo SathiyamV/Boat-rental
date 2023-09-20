b1f2a65664b45b051b410826266a5ca24d34d610
SathiyamV <sathiyamsharmila@gmail.com>
2023-09-20 13:28:44 +0530

    login-form

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-// const panels = document.querySelectorAll(".panel");
-
-// panels.forEach((panel) => {
-//   panel.addEventListener("click", () => {
-//     removeActiveClass();
-//     panel.classList.add("active");
-//   });
-// });
-
-// function removeActiveClass() {
-//   panels.forEach((panel) => {
-//     panel.classList.remove("active");
-//   });
-// }
-
+// image expanader
 const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) => {
@@ -27,3 +13,15 @@ function removeActiveClass() {
     panel.classList.remove("active");
   });
 }
+
+// login popup
+
+document.getElementById("show-login").addEventListener("click", function () {
+  document.querySelector(".pop-up-box").classList.add("popup");
+  document.querySelector(".popup-login").classList.add("active");
+});
+
+document.querySelector(".close-btn").addEventListener("click", function () {
+  document.querySelector(".pop-up-box").classList.remove("popup");
+  document.querySelector(".popup-login").classList.remove("active", "popup");
+});
